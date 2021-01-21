fb1bcde6c60de3c3678b3c7434a00e6d07256321
Kill-loli <77718689+Kill-loli@users.noreply.github.com>
2021-01-21 22:04:17 +0530

    Update afk.py

--- Cutiepii_Robot/modules/afk.py
+++ Cutiepii_Robot/modules/afk.py
@@ -57,8 +57,8 @@ def no_longer_afk(update: Update, context: CallbackContext):
         firstname = update.effective_user.first_name
         try:
             options = [
-                '{} Cutie Cutie!', '{} Yōkoso!', '{} welcome back.. no one missed you!!',
-                '{} why you came here?', '{} is wasting his time in this chat!', '{} welcome back...Now pay 100$ or get banned.',
+                '{} came again! ah shit, we were having fun without you!', '{} welcome back.. no one missed you!!',
+                '{} why you came here?', '{} we had fun without you!', '{} welcome back...Now pay 100$ or get banned.',
                 'Yamete...Yamete-kudasai {}-sama', 'Oh my! {} got no chills!!', '{} got a girlfriend! thats why he was afk.', '{} welcome to hell again.', '{} is here! start the show!', '{} bruh you should delete your telegram account.', '{} ahem! my love is here!', '{} damn... I saw you were online.. reading the messages.. but u were afk.', '{} yess.. lets start trashing the chat!', 'Spammer just arrived.. be ready everyone.. let me grab my ban-hammer!', '{} please be gentle with me... in the chat..'             
             ]
             chosen_option = random.choice(options)
